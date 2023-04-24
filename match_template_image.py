ddef7ebdc109d4f05d6ab3cd32a3a04438536cd2
Hosszú Szilárd <55355512+mentol555@users.noreply.github.com>
2023-04-24 17:53:04 +0300

    Update match_template_image.py

--- match_template_image.py
+++ match_template_image.py
@@ -2,7 +2,10 @@ import string
 import warnings
 import cv2
 import numpy as np
+import pytesseract
 from scipy import ndimage
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 warnings.filterwarnings('ignore')
 
@@ -31,46 +34,59 @@ def match_template(image, template):
             if(result[i, j]>1.0): result[i,j] = 1.0
     return result
 
-# TODO: Kiszámolni, hogy mekkora egy átlagos kártyánál a kis szimbólum(ami nekem a template)
-# TODO: és a teljes kártya aránya. Vehetjük ezt az arányt, hogy skálázzuk a bejövő képet.
-# TODO: Lehet h nem minden esetben működik, de szerintem elég sok esetben hasonló az arány..kb..
-# TODO: Ezután már úgy ahogy el lehet dönteni, hogy melyik szimbólumra adja a legjobb matchet vissza!
-# TODO: Forgatás? idk
+# multi scale symbol matching
+# 3 méretarányt kipróbál, minta szimbólumot illeszt,
+# a legjobban illeszkedőt választja
+def matchSymbol(image, template):
+    minVal = 100
+    for scale in (12, 15, 17):
+        # rescale, keep the ratio!
+        (h, w) = image.shape[:2]
+        height = template.shape[0] * scale
+        r = height / float(h)
+        width = int(w * r)
+        dim = (width, int(height))
+        # resize image
+        rescaled = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+        # csak a bal felso resze kell a kepnek!
+        resized = rescaled[:rescaled.shape[0] // 4, :rescaled.shape[1] // 4]
+
+        #result = cv2.matchTemplate(image_copy, template, cv2.TM_SQDIFF_NORMED)
+        result = match_template(resized, template)  #### cv2.matchTemplate func. TM_SQDIFF_NORMED
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        print(scale, min_val)
+        if(min_val < minVal):
+            minVal = min_val
+            results = (resized, result)
+    return results
+# defektes meg
+def matchCharacter(image, template):
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # csak a bal felso resze kell a kepnek!
+    resized = image_rgb[:image_rgb.shape[0] // 4, :image_rgb.shape[1] // 4]
+    resultCharacter = pytesseract.image_to_string(resized, config='--psm 10')
+    return resultCharacter
+
 def main():
-    image = cv2.imread('input/main_image_2.jpg')
+    # read image
+    image = cv2.imread('input/card1.png')
+    # rotate image to x degrees
     image = ndimage.rotate(image, 0)
-    # Convert copy of image to Grayscale format as we need that
-    # for template matching.
-
-    cv2.imshow('Original', image)
-    width = int(image.shape[1] * 60 / 100)
-    height = int(image.shape[0] * 60 / 100)
-    dim = (width, height)
-    # resize image
-    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-
-    ### csak a bal felso 1/3 kepet vizsgaljuk!!!
-    resized = resized[:resized.shape[0] // 3, :resized.shape[1] // 3]
-
-    image_copy = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
     # Read the template in grayscale format.
     template = cv2.imread('input/newtemplate2.jpg', 0)
-    width = int(template.shape[1] * 100 / 100)
-    height = int(template.shape[0] * 100 / 100)
-    dim = (width, height)
-    # resize image
-    newtemplate = cv2.resize(template, dim, interpolation=cv2.INTER_AREA)
-    w, h = newtemplate.shape[::-1]
+    w, h = template.shape[::-1]
 
-    # Apply template Matching.
-    #testimage = np.full((10, 10), [255, 100, 50, 255, 100, 50, 255, 100, 50, 30], dtype=np.uint8)
-    #testtempl = np.full((2, 2), 240, dtype=np.uint8)
+    cv2.imshow('Original', image)
 
-    result = cv2.matchTemplate(image_copy, newtemplate, cv2.TM_SQDIFF_NORMED)
-    #result = match_template(image_copy, newtemplate)  #### cv2.matchTemplate func. TM_SQDIFF_NORMED
+    # convert the image to grayscale
+    image_copy = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # scale the image and choose the best matching one
+    resizedImage, result = matchSymbol(image_copy, template)
+    print(matchCharacter(image, template))
 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
     print(min_val)
     treshold = 0.15
     if(min_val <= treshold):
@@ -82,14 +98,14 @@ def main():
     x1, y1 = min_loc # SQDIFF-nel minLoc, mashol maxLoc
     ## Bottom right x and y coordinates.
     x2, y2 = (x1 + w, y1 + h)
-    cv2.rectangle(resized, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    cv2.imshow('Resized image', resized)
-    cv2.imshow('Template', newtemplate)
+    cv2.rectangle(resizedImage, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.imshow('Resized image', resizedImage)
+    cv2.imshow('Template', template)
     ## Normalize the result for proper grayscale output visualization.
-    cv2.normalize(resized, result, 0, 1, cv2.NORM_MINMAX, -1)
+    cv2.normalize(resizedImage, result, 0, 1, cv2.NORM_MINMAX, -1)
     cv2.imshow('Detected point', result)
     cv2.waitKey(0)
-    cv2.imwrite('outputs/image_result.jpg', resized)
+    cv2.imwrite('outputs/image_result.jpg', resizedImage)
     cv2.imwrite('outputs/template_result.jpg', result * 255.)
 
 main()
